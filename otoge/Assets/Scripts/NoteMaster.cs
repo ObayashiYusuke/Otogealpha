437cc030999d26981ed60529f3061fac8c39d6df
ObayashiYusuke <theoba1210345@icloud.com>
2019-06-21 15:59:59 +0900

    BPM取得完了

--- otoge/Assets/Scripts/NoteMaster.cs
+++ otoge/Assets/Scripts/NoteMaster.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class NoteMaster : MonoBehaviour
 {
-	public int a = 4;
+	public int a = 4;//テスト生成個数
 	public GameObject Pref;
+
+	//　読む込むテキストが書き込まれている.txtファイル
+	[SerializeField]
+	private TextAsset textAsset;
+	//　Resourcesフォルダから直接テキストを読み込む
+	private string fumenAllText;
+	//　改行で分割して配列に入れる
+
+	private string[] splitText;
+
+	//　テキストの現在行番号
+	private int textNum = 0;
+	private int rowLength;
+
+	private float BPM = 1;
 
 	private IEnumerator Test1Coloutine()
 	{
@@ -20,8 +37,30 @@ public class NoteMaster : MonoBehaviour
 
 		Note n;
 
+
+
 		Debug.Log("Test2Coloutine開始");
-		while (a > 0)
+
+		fumenAllText = (Resources.Load("Test2", typeof(TextAsset)) as TextAsset).text;//テキストの読み込み
+
+		splitText = fumenAllText.Split(char.Parse("\n"));//テキストを改行ごとに分ける
+		rowLength = fumenAllText.Split('\n').Length;
+
+		BPM = GetBPM();
+
+		if(BPM <= 0)
+		{
+			Debug.Log("BPM is not found.");
+			BPM = 1;
+		}
+		else if (BPM > 0)
+		{
+			BPM = float.Parse(Regex.Replace(splitText[textNum], @"[bpm=]", ""));
+			Debug.Log("BPM IS " + BPM);
+		}
+
+
+		/*while (a > 0)
 		{
 			Debug.Log("あと" + a + "個生成");
 			var obj = Instantiate(Pref);
@@ -38,7 +77,26 @@ public class NoteMaster : MonoBehaviour
 
 			yield return Test1Coloutine();
 			a--;
+		}過去の遺産*/
+		yield return new WaitForSeconds(1f);
+
+	}
+
+	public float GetBPM()
+	{
+		while (Regex.IsMatch(splitText[textNum], "bpm") == false)//bpmが出でくるまで行を進める
+		{
+			textNum++;
+			if (textNum >= rowLength)//見つからず最後の行まで行ったら-1を返す
+			{
+				return -1;
+			}
 		}
+		if (splitText[textNum].Split('.').Length > 2)
+		{
+			return -1;
+		}
+		return float.Parse(Regex.Replace(splitText[textNum], @"[^0-9.]", ""));//BPMをフロート型にして返す
 
 	}
 
@@ -46,7 +104,10 @@ public class NoteMaster : MonoBehaviour
 	void Start()
     {
 		StartCoroutine(Test2Coroutine());
-    }
+
+		
+
+	}
 
     // Update is called once per frame
     void Update()
