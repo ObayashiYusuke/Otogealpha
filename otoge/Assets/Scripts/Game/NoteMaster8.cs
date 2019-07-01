6bbeb90cacf6690cd5c04c6e9d7bfc70d7ee2cb0
ObayashiYusuke <theoba1210345@icloud.com>
2019-07-02 02:00:17 +0900

    8レーン

--- otoge/Assets/Scripts/Game/NoteMaster8.cs
+++ otoge/Assets/Scripts/Game/NoteMaster8.cs
@@ -6,13 +6,13 @@ using System.Text.RegularExpressions;
 using UnityEngine.SceneManagement;
 
 
-public class NoteMaster : MonoBehaviour
+public class NoteMaster8 : MonoBehaviour
 {
 	public int life = 8;
 	public int score = 0;
 	public float speed = 10;
 	public GameObject Pref;
-	public int inputBuffer=0,inputBufferOld =0;//入力を数値化 oldはまだいらなかった
+	public int inputBuffer = 0, inputBufferOld = 0;//入力を数値化 oldはまだいらなかった
 	public int buttonnumber = 4;
 
 	public float greatJudge;
@@ -42,9 +42,9 @@ public class NoteMaster : MonoBehaviour
 
 
 	//譜面情報
-	private float waittime = 0;	//譜面が曲に対して遅れる時間
+	private float waittime = 0; //譜面が曲に対して遅れる時間
 	private float barTime;  //1小節の時間
-	private float endtime;	//開始から終了までの時間
+	private float endtime;  //開始から終了までの時間
 	private float starttime;//比較用の開始時刻記録用
 
 	//生成したノーツオブジェクトのリスト
@@ -55,7 +55,7 @@ public class NoteMaster : MonoBehaviour
 
 	private IEnumerator TestCoroutine()
 	{
-		
+
 
 		fumenAllText = (Resources.Load("Test8simple", typeof(TextAsset)) as TextAsset).text;//テキストの読み込み
 
@@ -66,7 +66,7 @@ public class NoteMaster : MonoBehaviour
 
 		BPM = GetBPM();
 
-		if(BPM <= 0)
+		if (BPM <= 0)
 		{
 			Debug.Log("BPM is not found.");
 			BPM = 1;
@@ -89,7 +89,7 @@ public class NoteMaster : MonoBehaviour
 		for (int i = 1; i <= 50; i++)
 		{
 			Debug.Log(i + "小節目の生成");
-			MakeOneBar(i,waittime,i);
+			MakeOneBar(i, waittime, i);
 		}
 
 
@@ -101,13 +101,13 @@ public class NoteMaster : MonoBehaviour
 		realWait = Time.time - starttime;
 		MusicPlay();
 
-		
-		
+
+
 	}
 
-	public float GetBPM()		//BPMを取得する関数!!!!!!! bpm=oo で記述
+	public float GetBPM()       //BPMを取得する関数!!!!!!! bpm=oo で記述
 	{
-		
+
 		if (SearchWord("bpm=") == false)
 		{
 			return -1;
@@ -120,9 +120,9 @@ public class NoteMaster : MonoBehaviour
 
 	}
 
-	public float GetWaittime()		//譜面再生までの時間を取得 waittime=oo で記述
+	public float GetWaittime()      //譜面再生までの時間を取得 waittime=oo で記述
 	{
-		if(SearchWord("waittime=") == false)
+		if (SearchWord("waittime=") == false)
 		{
 			return 0;
 		}
@@ -134,7 +134,7 @@ public class NoteMaster : MonoBehaviour
 
 	}
 
-	public float GetEndtime()	//開始から終了までの時間を取得する関数 endtime=ooで記述
+	public float GetEndtime()   //開始から終了までの時間を取得する関数 endtime=ooで記述
 	{
 		if (SearchWord("endtime=") == false)
 		{
@@ -152,7 +152,7 @@ public class NoteMaster : MonoBehaviour
 
 	public bool MakeOneBar(int skipBar, float wait, int barNumber)
 	{
-		Vector3 pos,size;
+		Vector3 pos, size;
 		pos.y = 0;
 		size.x = 0.3f;
 		size.y = 1;
@@ -163,7 +163,7 @@ public class NoteMaster : MonoBehaviour
 
 
 
-		if(SearchWord("--") == false)
+		if (SearchWord("--") == false)
 		{
 			return false;
 		}
@@ -180,12 +180,12 @@ public class NoteMaster : MonoBehaviour
 
 		float interval = barTime / (textNum - startline - 1);//(textNum - startline - 1)は行の数
 
-		for (int i = 0; (startline + 1 + i ) < textNum; i++)
+		for (int i = 0; (startline + 1 + i) < textNum; i++)
 		{
 			/*Debug.Log("小節の中の" + (i + 1) + "行目");*/
-			lineData = (Regex.Replace(splitText[startline + 1 + i], @"[^0-9A-Z]",""));
+			lineData = (Regex.Replace(splitText[startline + 1 + i], @"[^0-9A-Z]", ""));
 			/*Debug.Log("lineData is" + lineData);*/
-			for(int l = 0;l < buttonnumber; l++)
+			for (int l = 0; l < buttonnumber; l++)
 			{
 				if (lineData[l] != '0')
 				{
@@ -210,7 +210,7 @@ public class NoteMaster : MonoBehaviour
 			}
 
 
-			
+
 
 		}
 
@@ -225,7 +225,7 @@ public class NoteMaster : MonoBehaviour
 		{
 			a = (a * 2) + 1;
 		}
-		for(int i = 1; left >= i; i++)
+		for (int i = 1; left >= i; i++)
 		{
 			a *= 2;
 		}
@@ -234,8 +234,8 @@ public class NoteMaster : MonoBehaviour
 
 
 	public bool SearchWord(string str)
-		/*文字列を検索し、最初のその文字列が見つかるまでtextNumを進める
-		 見つかればtrue,見つからなければfalse*/
+	/*文字列を検索し、最初のその文字列が見つかるまでtextNumを進める
+	 見つかればtrue,見つからなければfalse*/
 	{
 		int rowfrom = textNum;
 		if (textNum >= rowLength)//見つからず最後の行まで行ったら失敗
@@ -257,7 +257,7 @@ public class NoteMaster : MonoBehaviour
 
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
 		starttime = Time.time;
 
 		StartCoroutine(TestCoroutine());
@@ -270,13 +270,12 @@ public class NoteMaster : MonoBehaviour
 	private void FixedUpdate()
 	{
 		Note note;
-		int sub = 0; 
+		int sub = 0;
 		float nowTime;
 
 		//inputBuffer = inputBufferOld;
 		inputBuffer = 0;
-		/*
-		//4ボタン
+
 		if (Input.GetKeyDown(KeyCode.S))
 		{
 			inputBuffer += 1;
@@ -293,41 +292,6 @@ public class NoteMaster : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.L))
 		{
 			inputBuffer += 8;
-		}
-		*/
-		if (Input.GetKeyDown(KeyCode.S))
-		{
-			inputBuffer += 1;
-		}
-
-		if (Input.GetKeyDown(KeyCode.D))
-		{
-			inputBuffer += 2;
-		}
-		if (Input.GetKeyDown(KeyCode.F))
-		{
-			inputBuffer += 4;
-		}
-		if (Input.GetKeyDown(KeyCode.G))
-		{
-			inputBuffer += 8;
-		}
-		if (Input.GetKeyDown(KeyCode.H))
-		{
-			inputBuffer += 16;
-		}
-
-		if (Input.GetKeyDown(KeyCode.J))
-		{
-			inputBuffer += 32;
-		}
-		if (Input.GetKeyDown(KeyCode.K))
-		{
-			inputBuffer += 64;
-		}
-		if (Input.GetKeyDown(KeyCode.L))
-		{
-			inputBuffer += 128;
 		}
 
 		if (inputBuffer != 0)
@@ -340,7 +304,7 @@ public class NoteMaster : MonoBehaviour
 
 			sub = noteList.FindIndex(x => x.time <= nowTime + goodJudge && x.time >= nowTime - goodJudge
 				&& (x.noteType & inputBuffer) != 0);
-			if(sub != -1)
+			if (sub != -1)
 			{
 				note = noteList[sub];
 				Debug.Log("note.time is " + note.time);
@@ -350,7 +314,7 @@ public class NoteMaster : MonoBehaviour
 					score += 100;
 					Debug.Log("GREAT");
 				}
-				else if(note.time > nowTime)
+				else if (note.time > nowTime)
 				{
 					score += 50;
 					Debug.Log("FAST");
@@ -362,9 +326,9 @@ public class NoteMaster : MonoBehaviour
 				}
 				Destroy(note.gameObject);//破壊
 
-										 noteList.RemoveAt(sub);
+				noteList.RemoveAt(sub);
 			}
-			
+
 
 		}
 
@@ -373,17 +337,17 @@ public class NoteMaster : MonoBehaviour
 
 	// Update is called once per frame
 	void Update()
-    {
+	{
 		scoreText.text = "Score : " + score.ToString();
 		lifeText.text = "Life : " + life.ToString();
 
 
-		if((Time.time - starttime) > endtime)
+		if ((Time.time - starttime) > endtime)
 		{
 			Finish();
 		}
 
-		
+
 	}
 
 	public void MusicPlay()
