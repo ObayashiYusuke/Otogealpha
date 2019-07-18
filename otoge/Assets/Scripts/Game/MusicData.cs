d589a64eb5e8a5067d67f2167c7822fea51c64c8
ObayashiYusuke <theoba1210345@icloud.com>
2019-07-18 17:49:58 +0900

    ぼたんがつくれたよ！！

---
+++ otoge/Assets/Scripts/Game/MusicData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicData : MonoBehaviour
+{
+
+	public string noteData;//譜面の文字列データ
+	public string musicTitle;//曲のタイトル
+	public float BPM;
+	public float playTime;//プレイ時間
+
+
+    
+}
